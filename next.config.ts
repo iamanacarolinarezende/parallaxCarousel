01e15ebbc80f3de4b992e538076c0a28771d91d2
iamanacarolinarezende <pluck_carol@hotmail.com>
2025-03-29 09:54:35 -0400

    BugFix on Routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_URL: "https://67e7f91d20e3af747c400c56.mockapi.io/",
+  },
 };
 
 export default nextConfig;
