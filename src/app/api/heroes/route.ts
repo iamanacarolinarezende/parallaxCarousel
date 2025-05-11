5e576cbef6a91b091fa25ee2f3fece6f219ec9f2
iamanacarolinarezende <pluck_carol@hotmail.com>
2025-05-11 10:44:52 -0400

    Routes Fix

--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -1,20 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    const res = await fetch(`${process.env.API_URL}/heroes`);
-    if (!res.ok) {
-      return NextResponse.json(
-        { error: "Failed to fetch heroes" },
-        { status: 500 }
-      );
-    }
-    const data = await res.json();
-    return NextResponse.json({ data });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Internal server error" },
-      { status: 500 }
-    );
-  }
+  const res = await fetch(`${process.env.API_URL}/heroes`);
+  const data = await res.json();
+
+  return NextResponse.json({ data });
 }
